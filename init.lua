688dc9547f82bef204b251213a22a300eaa74982
Jörgen Gullstrand <41142640+moruscerberus@users.noreply.github.com>
2026-03-24 19:39:08 +0100

    Added files via first upload

---
+++ init.lua
@@ -0,0 +1,85 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- ======================
+-- Keymaps
+-- ======================
+
+-- move line up/down (normal mode)
+vim.keymap.set("n", "<A-Up>", ":m .-2<CR>==")
+vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==")
+
+-- move selected lines (visual mode)
+vim.keymap.set("v", "<A-Up>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
+
+-- ctrl+k cut selection / line
+vim.keymap.set("v", "<C-k>", '"+d')
+vim.keymap.set("n", "<C-k>", '"+dd')
+
+-- tab indent in visual mode
+vim.keymap.set("v", "<Tab>", ">gv")
+vim.keymap.set("v", "<S-Tab>", "<gv")
+vim.keymap.set("v", "<kBackTab>", "<gv")
+
+-- optional: tab / shift-tab in normal mode
+vim.keymap.set("n", "<Tab>", ">>")
+vim.keymap.set("n", "<S-Tab>", "<<")
+vim.keymap.set("n", "<kBackTab>", "<<")
+
+-- ======================
+-- lazy.nvim bootstrap
+-- ======================
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--branch=stable",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- ======================
+-- Plugins
+-- ======================
+
+require("lazy").setup({
+  {
+    "neovim/nvim-lspconfig",
+    version = "v1.8.0",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      require("lspconfig").rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-n>"] = cmp.mapping.select_next_item(),
+          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+})
